b53520139f0bd3b98d2a5da459865bad9294aa76
Yago M. Castro <yagomcastro1@gmail.com>
2025-02-12 10:56:22 -0300

    Read directly from the zip file

--- scripts/analisa_temperatura.py
+++ scripts/analisa_temperatura.py
@@ -2,8 +2,18 @@ import sys
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
+import zipfile
+
 
 # Lê os dados do arquivo especificado em 'file_path', calcula a variação de temperatura
 # e exibe o nome do país seguido da temperatura correspondente.
-def analyze_temperature(file_path):
-    df = pd.read_csv(file_path)
+def analyze_temperature(zip_path, file_name):
+    with zipfile.ZipFile(zip_path, 'r') as zip_file:
+        df = pd.read_csv(zip_file.open(file_name), skiprows=5)
+        print(df)
+
+
+zip_path = sys.argv[1]
+file_name = sys.argv[2]
+
+analyze_temperature(zip_path, file_name)
