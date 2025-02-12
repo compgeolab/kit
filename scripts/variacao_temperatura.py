b7238d1f6f844d58a705da86d383a37dc71c1eca
Yago M. Castro <yagomcastro1@gmail.com>
2025-02-12 13:32:11 -0300

    Rename function

--- scripts/variacao_temperatura.py
+++ scripts/variacao_temperatura.py
@@ -6,7 +6,7 @@ import zipfile
 
 # Lê os dados do arquivo especificado em 'file_path', calcula a variação de temperatura
 # e exibe o nome do país seguido da temperatura correspondente.
-def analyze_temperature(zip_path, file_name):
+def analyze_temperature_variation(zip_path, file_name):
     with zipfile.ZipFile(zip_path, 'r') as zip_file:
         df = pd.read_csv(zip_file.open(file_name), skiprows=5)
         temperature = df['temperature_C']
@@ -15,4 +15,4 @@ def analyze_temperature(zip_path, file_name):
 zip_path = sys.argv[1]
 file_name = sys.argv[2]
 
-analyze_temperature(zip_path, file_name)
+analyze_temperature_variation(zip_path, file_name)
