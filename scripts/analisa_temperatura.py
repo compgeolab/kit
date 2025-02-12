d649a46b6f593c8555a1e6d98997144f09034c1e
Yago M. Castro <yagomcastro1@gmail.com>
2025-02-12 11:07:38 -0300

    Write analize_temperature script

--- scripts/analisa_temperatura.py
+++ scripts/analisa_temperatura.py
@@ -10,8 +10,8 @@ import zipfile
 def analyze_temperature(zip_path, file_name):
     with zipfile.ZipFile(zip_path, 'r') as zip_file:
         df = pd.read_csv(zip_file.open(file_name), skiprows=5)
-        print(df)
-
+        temperature = df['temperature_C']
+        print('%s | %.2f' % (file_name[:-4].upper(), temperature.max() - temperature.min()))
 
 zip_path = sys.argv[1]
 file_name = sys.argv[2]
