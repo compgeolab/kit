72e69d918d95f1eec479e8bb19c694be49e6e9f5
Yago M. Castro <yagomcastro1@gmail.com>
2025-02-12 14:16:21 -0300

    Read all data

---
+++ scripts/analisar_temperatura.py
@@ -0,0 +1,17 @@
+import sys
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+import zipfile
+
+zip_path = sys.argv[1]
+file_name = sys.argv[2]
+
+with zipfile.ZipFile(zip_path, 'r') as zip_file:
+    count = 1
+    print(len(zip_file.namelist()))
+    for file in zip_file.namelist():
+        if file != 'README.md':
+            print(file)
+            df = pd.read_csv(zip_file.open(file), skiprows=5)
+            print(df)
