1a5ab556fa80381f95685bf8b407b131cc661a94
YagoMCastro <yagomcastro1@gmail.com>
2025-02-13 16:26:17 -0300

    Comment the code

--- code/variacao_temperatura.py
+++ code/variacao_temperatura.py
@@ -14,13 +14,15 @@ import numpy as np
 # O caminho para o arquivo é recebido pela linha de comando
 file_path = pathlib.Path(sys.argv[1])
 
+# Read the csv ignoring the comments 
 data = pd.read_csv(file_path, comment="#")
 
-last_ten_years = data.year_decimal > data.year_decimal.iloc[-1] - 5
+# Filter data from the last five year
+last_five_years = data.year_decimal > data.year_decimal.iloc[-1] - 5
 
 # Regressão linear
 coefficients = np.polyfit(
-    data.year_decimal[last_ten_years], data.temperature_C[last_ten_years], 1
+    data.year_decimal[last_five_years], data.temperature_C[last_five_years], 1
 )
 
 # Impressão de resultados
